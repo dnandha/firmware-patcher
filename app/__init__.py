ccbb155a4b2e4467b1e25d529e975dd5b7125598
douche <douche@douche.com>
2022-02-05 21:55:27 +0100

    update layout

--- app/__init__.py
+++ app/__init__.py
@@ -10,8 +10,6 @@ import pathlib
 from patcher import FirmwarePatcher
 
 app = flask.Flask(__name__)
-app.config['UPLOAD_FOLDER'] = os.path.join(os.path.dirname(pathlib.Path(__file__).parent.absolute()), 'bin')
-
 
 @app.errorhandler(Exception)
 def handle_bad_request(e):
@@ -40,56 +38,55 @@ def home():
     return flask.render_template('home.html')
 
 
-@app.route('/cfw')
+@app.route('/cfw', methods = ['POST'])
 def patch_firmware():
-    version = flask.request.args.get('version', None)
+    version = flask.request.form.get('version', None)
     if version not in ['DRV236', 'DRV304']:
-        return 'Invalid firmware version.', 400
+        return 'Ungültige Firmware Version.', 400
 
     f = flask.request.files['filename']
-    f.save(f.filename)
-    print("file saved")
+    if f.filename != f'{version}.bin':
+        return 'Falsche oder keine Datei ausgewählt.', 400
 
-    with open('{}/{}.bin'.format(app.config["BINS"], version), 'rb') as fp:
-        patcher = FirmwarePatcher(fp.read())
+    patcher = FirmwarePatcher(f.read())
 
-    brakelight_mod = flask.request.args.get('brakelight_mod', None)
+    brakelight_mod = flask.request.form.get('brakelight_mod', None)
     if brakelight_mod:
         patcher.brakelight_mod()
 
-    speed_plus2 = flask.request.args.get('speed_plus2', None)
+    speed_plus2 = flask.request.form.get('speed_plus2', None)
     if speed_plus2:
         patcher.speed_plus2()
 
-    remove_autobrake = flask.request.args.get('remove_autobrake', None)
+    remove_autobrake = flask.request.form.get('remove_autobrake', None)
     if remove_autobrake:
         patcher.remove_autobrake()
 
-    remove_kers = flask.request.args.get('remove_kers', None)
+    remove_kers = flask.request.form.get('remove_kers', None)
     if remove_kers:
         patcher.remove_kers()
 
-    motor_start_speed = flask.request.args.get('motor_start_speed', None)
+    motor_start_speed = flask.request.form.get('motor_start_speed', None)
     if motor_start_speed is not None:
         motor_start_speed = float(motor_start_speed)
         assert motor_start_speed >= 0 and motor_start_speed <= 100
         patcher.motor_start_speed(motor_start_speed)
 
-    remove_charging_mode = flask.request.args.get('remove_charging_mode', None)
+    remove_charging_mode = flask.request.form.get('remove_charging_mode', None)
     if remove_charging_mode:
         patcher.remove_charging_mode()
 
-    speed_params = flask.request.args.get('speed_params', None)
+    speed_params = flask.request.form.get('speed_params', None)
     if speed_params:
-        speed_ampere = int(flask.request.args.get('speed_ampere', None))
+        speed_ampere = int(flask.request.form.get('speed_ampere', None))
         assert speed_ampere >= 0 and speed_ampere <= 65535
-        normal_ampere = int(flask.request.args.get('normal_ampere', None))
+        normal_ampere = int(flask.request.form.get('normal_ampere', None))
         assert normal_ampere >= 0 and normal_ampere <= 65535
-        eco_ampere = int(flask.request.args.get('eco_ampere', None))
+        eco_ampere = int(flask.request.form.get('eco_ampere', None))
         assert eco_ampere >= 0 and eco_ampere <= 65535
         patcher.speed_params(eco_ampere, normal_ampere, speed_ampere)
 
-    dpc = flask.request.args.get('dpc', None)
+    dpc = flask.request.form.get('dpc', None)
     if dpc:
         patcher.dpc()
 
