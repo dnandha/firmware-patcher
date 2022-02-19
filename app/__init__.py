8005f70d60956ad17edf10abcdc7755c224ae107
douche <douche@douche.com>
2022-02-19 18:13:46 +0100

    add zip button

--- app/__init__.py
+++ app/__init__.py
@@ -60,87 +60,106 @@ def home():
 def patch_firmware():
     f = flask.request.files['filename']
 
+    patchorzip = flask.request.form.get('patchorzip', None)
+    patch = patchorzip == "Patch!"
+
     data = f.read()
-    md5 = hashlib.md5(data).hexdigest()
-    if md5 == '194df3966443d8a913b47006119d45d9':
+    if not len(data) > 0xf:
+        return 'Keine Datei ausgewählt.', 400
+
+    try:
+        scoot = data[0x100:0x10f].decode('ascii')
+    except:
+        return 'Falsche Datei ausgewählt.', 400
+
+    model = ""
+    version = ""
+    if scoot == "Scooter_MiP2_V0":
+        model = "pro2"
         version = "DRV236"
-    elif md5 == '682fc3b89122ff0a21ebe2fa3ada4908':
+    elif scoot == "Scooter_Mi1S_V0":
+        model = "1s"
         version = "DRV304"
     else:
-        return 'Falsche oder keine Datei ausgewählt.', 400
+        return 'Falsche Datei ausgewählt.', 400
 
     # make zip file for firmware
     zip_buffer = io.BytesIO()
     zip_file = zipfile.ZipFile(zip_buffer, 'a', zipfile.ZIP_DEFLATED, False)
 
     patcher = FirmwarePatcher(data)
+    if patch:
+        md5 = hashlib.md5(data).hexdigest()
+        if not ((version == "DRV236" and md5 == '194df3966443d8a913b47006119d45d9')
+            or (version == "DRV304" and md5 == '682fc3b89122ff0a21ebe2fa3ada4908')):
+            return 'Datei bereits gepatcht. Zum Patchen bitte Vanilla Datei auswählen.', 400
 
-    ltgm = flask.request.form.get('ltgm', None)
-    if ltgm:
-        print("ltgm")
-        patcher.ltgm()
+        ltgm = flask.request.form.get('ltgm', None)
+        if ltgm:
+            print("ltgm")
+            patcher.ltgm()
 
-    dpc = flask.request.form.get('dpc', None)
-    if dpc:
-        print("dpc")
-        patcher.dpc()
+        dpc = flask.request.form.get('dpc', None)
+        if dpc:
+            print("dpc")
+            patcher.dpc()
 
-    brakelight_mod = flask.request.form.get('brakelight_mod', None)
-    if brakelight_mod:
-        print("blm")
-        patcher.brakelight_mod()
+        brakelight_mod = flask.request.form.get('brakelight_mod', None)
+        if brakelight_mod:
+            print("blm")
+            patcher.brakelight_mod()
 
-    speed_plus2 = flask.request.form.get('speed_plus2', None)
-    if speed_plus2:
-        print("spt")
-        patcher.speed_plus2()
+        speed_plus2 = flask.request.form.get('speed_plus2', None)
+        if speed_plus2:
+            print("spt")
+            patcher.speed_plus2()
 
-    speed_plus2_global = flask.request.form.get('speed_plus2_global', None)
-    if speed_plus2_global:
-        print("sptg")
-        patcher.speed_plus2(True)
+        speed_plus2_global = flask.request.form.get('speed_plus2_global', None)
+        if speed_plus2_global:
+            print("sptg")
+            patcher.speed_plus2(True)
 
-    remove_autobrake = flask.request.form.get('remove_autobrake', None)
-    if remove_autobrake:
-        print("ra")
-        patcher.remove_autobrake()
+        remove_autobrake = flask.request.form.get('remove_autobrake', None)
+        if remove_autobrake:
+            print("ra")
+            patcher.remove_autobrake()
 
-    remove_kers = flask.request.form.get('remove_kers', None)
-    if remove_kers:
-        print("rk")
-        patcher.remove_kers()
+        remove_kers = flask.request.form.get('remove_kers', None)
+        if remove_kers:
+            print("rk")
+            patcher.remove_kers()
 
-    motor_start_speed = flask.request.form.get('motor_start_speed', None)
-    if motor_start_speed is not None:
-        print("mss", motor_start_speed)
-        motor_start_speed = float(motor_start_speed)
-        assert motor_start_speed >= 0 and motor_start_speed <= 100
-        patcher.motor_start_speed(motor_start_speed)
+        motor_start_speed = flask.request.form.get('motor_start_speed', None)
+        if motor_start_speed is not None:
+            print("mss", motor_start_speed)
+            motor_start_speed = float(motor_start_speed)
+            assert motor_start_speed >= 0 and motor_start_speed <= 100
+            patcher.motor_start_speed(motor_start_speed)
 
-    remove_charging_mode = flask.request.form.get('remove_charging_mode', None)
-    if remove_charging_mode:
-        print("rc")
-        patcher.remove_charging_mode()
+        remove_charging_mode = flask.request.form.get('remove_charging_mode', None)
+        if remove_charging_mode:
+            print("rc")
+            patcher.remove_charging_mode()
 
-    wheelsize = flask.request.form.get('wheelsize', None)
-    if wheelsize is not None:
-        print("ws", wheelsize)
-        wheelsize = float(wheelsize)
-        assert wheelsize >= 0 and wheelsize <= 100
-        mult = wheelsize/8.5  # 8.5" is default
-        patcher.wheel_speed_const(mult)
+        wheelsize = flask.request.form.get('wheelsize', None)
+        if wheelsize is not None:
+            print("ws", wheelsize)
+            wheelsize = float(wheelsize)
+            assert wheelsize >= 0 and wheelsize <= 100
+            mult = wheelsize/8.5  # 8.5" is default
+            patcher.wheel_speed_const(mult)
 
-    thirtyamps = flask.request.form.get('thirtyamps', None)
-    if thirtyamps:
-        print("amp")
-        patcher.ampere(30000)
+        thirtyamps = flask.request.form.get('thirtyamps', None)
+        if thirtyamps:
+            print("amp")
+            patcher.ampere(30000)
 
-    shutdown_time = flask.request.form.get('shutdown_time', None)
-    if shutdown_time is not None:
-        print("st", shutdown_time)
-        shutdown_time = float(shutdown_time)
-        assert shutdown_time >= 0 and shutdown_time <= 5
-        patcher.shutdown_time(shutdown_time)
+        shutdown_time = flask.request.form.get('shutdown_time', None)
+        if shutdown_time is not None:
+            print("st", shutdown_time)
+            shutdown_time = float(shutdown_time)
+            assert shutdown_time >= 0 and shutdown_time <= 5
+            patcher.shutdown_time(shutdown_time)
 
     zip_file.writestr('FIRM.bin', patcher.data)
     md5 = hashlib.md5()
@@ -152,7 +171,7 @@ def patch_firmware():
     md5e.update(patcher.data)
 
     info_txt = 'dev: {};\nnam: {};\nenc: B;\ntyp: DRV;\nmd5: {};\nmd5e: {};\n'.format(
-        "pro2" if version == "DRV236" else "1s", version, md5.hexdigest(), md5e.hexdigest())
+        model, version, md5.hexdigest(), md5e.hexdigest())
 
     zip_file.writestr('info.txt', info_txt.encode())
     zip_file.comment = flask.request.url.encode()
